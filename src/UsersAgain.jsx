362e4efc363d330262bf4cdacc01ab4b370835f2
Peter-Adjei <95479664+Peter-Adjei@users.noreply.github.com>
2023-09-18 12:33:55 -0700

    Add files via upload

---
+++ src/UsersAgain.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+const UsersAgain = () => {
+    let myUsers = [];
+    const [users, setUsers] = useState(myUsers);
+    
+    useEffect(() =>{
+        axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) =>{
+            console.log(res);
+            setUsers(res.data)
+
+        })
+        .catch((err) =>  console.log(err));
+
+
+    }, []);
+
+    return (
+        <div>
+            <h1>All Users</h1>
+            <hr/>
+            <div>
+                    {users.map((user) => (
+                        <div>
+                            <p>{user.name}</p>
+                            <p>{user.username}</p>
+                            <p>{user.email}</p>
+                            <hr />
+                        </div>
+                    ))}
+
+                </div>
+        </div>
+    );
+}
+
+export default UsersAgain;
